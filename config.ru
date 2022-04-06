a00ca4341942ce76c94dfcfda29c0ba568a20516
nonnax <ironald@gmail.com>
2022-04-06 13:20:32 +0800

    working rest

--- config.ru
+++ config.ru
@@ -2,9 +2,18 @@
 # Id$ nonnax 2022-04-03 12:02:20 +0800
 require_relative 'lib/ala'
 
+
 get '/' do
   template = File.read('views/template.erb')
   erb template, locals: {name: params[:name]}
+end
+
+post '/' do  
+  [:posting, params[:name]].join("\t")
+end
+
+get '/r' do  
+  res.redirect '/'
 end
 
 post '/hey' do
--- lib/ala.rb
+++ lib/ala.rb
@@ -1,53 +1,47 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-04-03 11:59:22 +0800
-%w[rack tilt delegate].map{|l|
-  require l
-}
-
-$handler='thin'
-$port=9292
+%w[rack tilt delegate].map{|l| require l }
+$handler, $port= 'thin', 9292
+D = Object.method(:define_method)
 
 Ala=Module.new do
   extend self
-  app, req=Rack::Builder.new
-
-  %w[get post].map do |m|
-    define_method(m){ |u, &b|
-      app.map(u) do
-        run ->(env){[200, {"Content-Type" => "text/html"}, [app.instance_eval(&b)]]}
-      end
-    }
+  app, routes, resp, req=Rack::Builder.new, Hash.new{|h, k| h[k]=[] },nil,nil 
+  D.(:resolve){|e| routes[e.values_at('REQUEST_METHOD', 'REQUEST_PATH')]}
+  D.(:res){ resp }
+  
+  %w[get post put delete].map do |m| 
+    D.(m){ |u, &b| 
+      routes[[m.upcase, u]] = b 
+      app.map(u) do 
+        run ->(e){
+            body = app.instance_eval(&resolve(e)) rescue nil            
+            res.write body
+            return res.finish if body
+            [404, {}, ['Not found']]
+          } 
+      end 
+    } 
   end
   
-  Tilt.default_mapping.lazy_map.each do |ext, engines|
-    define_method(ext) do |arg, *args|
+  Tilt.lazy_map.each do |ext, engines|
+    D.(ext) do |arg, *args|
       arg=File.read(File.expand_path("../views/#{arg}.erb", __dir__)) if arg.is_a?(Symbol)      
-      Kernel.const_get(engines.last.first).new(*args){ arg }
-      .then{|template|template.render(app, locals=args.grep(Hash).pop.fetch(:locals, {})) }
+      Tilt.template_for(ext).new(*args){ arg }
+      .then{|t|t.render(app, locals=args.grep(Hash).pop.fetch(:locals, {})) }
     end
   end
- # locals = (args[0].respond_to?(:[]) ? args[0][:locals] : nil) || {}    # (args, args[0]&.[](:locals) || {})
     
-  define_method(:run!) do
-    Rack::Handler.get($handler).run( app, Port:$port ) {|server| $server=server }
-  end
+  D.(:run!){ Rack::Handler.get($handler).run( app, Port:$port ) {|server| $server=server } }
   
-  %w[params session].map{ |method| define_method( method){ req.send method} }
-  
-  %w[set enable disable configure helpers use register].map{|m|define_method(m){|*_,&b|b&.[]}} 
-  
-  # app.use Rack::Session::Cookie, secret: '__a_very_Very_lo0Ong_sess1on_str1ng__'
-  
-  define_method(:before) do |&b|
-    app.use Rack::Config, &b
-  end
+  %w[params session].map{ |method| D.( method){ req.send method} }
+  %w[set enable disable configure helpers use register].map{|m|D.(m){|*_,&b|b&.[]}} 
+  D.(:before){ |&b| app.use Rack::Config, &b}
 
-  app.use Rack::Lock
-  
-  before do |e|
-    req = Rack::Request.new e
-    req.params.dup.map do |k, v|
-      params[k.to_sym] = v
-    end
-  end
+  app.use Rack::Lock 
+  before do |e| 
+    req = Rack::Request.new(e); params.merge! req.params 
+    resp = Rack::Response.new
+  end  
+  self
 end.then{|x| include x }
